d90ea70d4e037b78d745378afb23d8675a4a8959
JaviCodx <javiersaizcano@gmail.com>
2022-01-30 12:17:52 +0100

    init docker compose infra

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,6 +1,14 @@
+import mongoose from 'mongoose'
 import { app } from './app'
 
+
 const start = async () => {
+    try {
+        await mongoose.connect("mongodb://mongodb:27017", {})
+        console.log('Connected to MongoDB')
+      } catch (err) {
+        console.log(err)
+      }
 
     app.listen(3010, () => {
         console.log('Listening on port 3010!!')
